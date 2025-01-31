9674867973821894f9e5dad0203e1ce53381c5d2
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-01-31 20:41:09 +0200

    feat: add a custom button

---
+++ src/components/custom-button.tsx
@@ -0,0 +1,55 @@
+import { useRef } from 'react'
+import gsap from 'gsap'
+
+type CustomButtonProps = {
+  children: string
+  styles: string
+}
+
+const CustomButton = ({ children, styles }: CustomButtonProps) => {
+  const ref = useRef(null)
+
+  const onMouseMoveHandler = (e: React.MouseEvent<HTMLElement>) => {
+    const rect = (e.target as HTMLElement).getBoundingClientRect()
+    gsap.to(ref.current, {
+      x: e.clientX - rect.left - window.innerWidth / 22,
+      y: e.clientY - rect.top - window.innerWidth / 100,
+      duration: 0.2
+    })
+  }
+
+  const onMouseOverHandler = () => {
+    gsap.to(ref.current, {
+      opacity: 1,
+      scale: 1,
+      duration: 0.4
+    })
+  }
+
+  const onMouseLeaveHandler = () => {
+    gsap.to(ref.current, {
+      opacity: 0,
+      scale: 0,
+      duration: 0.4
+    })
+  }
+  return (
+    <button
+      className={
+        'px-[1.1vw] py-[.43vw] bg-black/20 flex items-center text-[.8vw] border-[0.09vw] border-white rounded-[.15vw] rounded-tl-[.6vw] rounded-br-[.6vw] cursor-pointer select-none relative overflow-hidden ' +
+        styles
+      }
+      onMouseOver={onMouseOverHandler}
+      onMouseLeave={onMouseLeaveHandler}
+      onMouseMove={onMouseMoveHandler}
+    >
+      <div
+        ref={ref}
+        className="w-[3vw] h-[3vw] bg-black absolute pointer-events-none rounded-full blur-[1vw] opacity-0 -z-20"
+      ></div>
+      {children}
+    </button>
+  )
+}
+
+export default CustomButton
